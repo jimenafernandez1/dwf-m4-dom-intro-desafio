55dc092f95804c8113cbf1e95d90e044a83a831d
Jimena <jimenafernandez.business@gmail.com>
2022-04-01 01:38:20 -0300

    Nueva lista de cosas

--- index.js
+++ index.js
@@ -25,6 +25,22 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+	const listaEl = document.querySelector(".lista");
+	const liEls = document.querySelectorAll(".lista li");
+	// eliminar li
+	for (let i = 0; i < liEls.length; i++) {
+		liEls[i].remove();
+		console.log("li eliminado");
+	}
+	// agregar li
+	for (let i = 0; i < cosasQueAprendimos.length; i++) {
+		const element = cosasQueAprendimos[i];
+		const newLiEl = document.createElement("li");
+		newLiEl.className = element.class;
+		newLiEl.textContent = element.tema;
+		listaEl.appendChild(newLiEl);
+	}
+}
 
 main();
